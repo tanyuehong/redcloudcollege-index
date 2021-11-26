b5c33ca2ef8b84cd766a09386234dea2e46de69d
tanyuehong <tanyuehong@qq.com>
2021-11-27 07:51:48 +0800

    请求地址

--- utils/serverRq.js
+++ utils/serverRq.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // 创建axios实例
 const service = axios.create({
-  baseURL: 'http://localhost/api', // api的base_url
+  baseURL: 'https://wwww.redskt.com/api', // api的base_url
   timeout: 20000, // 请求超时时间
 })
 

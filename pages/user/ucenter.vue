08164b73285a29f2dcf3b7ad5e6d802f45fd072e
tanyuehong <tanyuehong@zuoyebang.com>
2021-12-23 14:48:03 +0800

    用户中心开发

---
+++ pages/user/ucenter.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="golobal_content">
+
+<div class="ucenter-home-header">
+
+<div class="header-backgroud">
+</div>
+
+</div>
+
+<div class="container">
+<div class="ucenter-home-content">
+  <el-tabs tab-position="left" style="height: 200px;">
+    <el-tab-pane label="学习进度">学习进度</el-tab-pane>
+    <el-tab-pane label="我的收藏">我的收藏</el-tab-pane>
+    <el-tab-pane label="我的文章">我的文章</el-tab-pane>
+    <el-tab-pane label="我的评论">我的评论</el-tab-pane>
+    <el-tab-pane label="个人设置">个人设置</el-tab-pane>
+  </el-tabs>
+</div>
+</div>
+
+</div>
+</div>
+
+</template>
+
+<style>
+
+.header-backgroud {
+    background-image: url(https://static.redskt.com/assets/img/usercover.jpg);
+    width: 80%;
+    background-position: center;
+    height: 180px;
+    background-size:100% 100%;
+}
+
+</style>

587676a801400dd5b4325bdc7648c3a78126eeb2
tanyuehong <tanyuehong@qq.com>
2022-05-25 10:42:27 +0800

    收藏代码提交

--- api/user.js
+++ api/user.js
@@ -1,94 +1,107 @@
-import request from "@/utils/request";
+import request from '@/utils/request'
 
 export default {
-
   // 获取用户文章列表
-  getUserArticleList(uId) { 
+  getArticleList(uId) {
     return request({
-      url: `/home/ucenter/getUserArticleList/${uId}`,
-      method: "get"
-    });
+      url: `/home/ucenter/getArticleList/${uId}`,
+      method: 'get',
+    })
+  },
+
+  getCollectArticleList(uId) {
+    return request({
+      url: `/home/ucenter/getCollectArticleList/${uId}`,
+      method: 'get',
+    })
+  },
+
+  getCollectAskList(uId) {
+    return request({
+      url: `/home/ucenter/getCollectAskList/${uId}`,
+      method: 'get',
+    })
   },
 
   //登录的方法
   submitLoginUser(userInfo) {
     return request({
       url: `/ucenter/login`,
-      method: "post",
-      data: userInfo
-    });
+      method: 'post',
+      data: userInfo,
+    })
   },
 
   //根据token获取用户信息
   getLoginUserInfo() {
     return request({
       url: `/ucenter/getUserInfo`,
-      method: "get"
-    });
+      method: 'get',
+    })
   },
   // 关注的方法
   addUserFocus(fid) {
     return request({
       url: `/ucenter/addUserFocus/${fid}`,
-      method: "get"
-    });
+      method: 'get',
+    })
   },
 
   // 取消关注的方法
   cancleUserFocus(fid) {
     return request({
       url: `/ucenter/cancleUserFocus/${fid}`,
-      method: "get"
-    });
+      method: 'get',
+    })
   },
 
   getUserFocus(fid) {
     return request({
       url: `/home/ucenter/getUserFocus/${fid}`,
-      method: "get"
-    });
+      method: 'get',
+    })
   },
 
   //根据手机号发验证码
   sendCode(phone) {
     return request({
       url: `/edumsm/msm/send/${phone}`,
-      method: "get"
-    });
+      method: 'get',
+    })
   },
 
   //注册的方法
   registerMember(formItem) {
     return request({
       url: `/home/ucenter/register`,
-      method: "post",
-      data: formItem
-    });
+      method: 'post',
+      data: formItem,
+    })
   },
 
   //根据token获取用户信息
   updateUserInfo(userData) {
     return request({
       url: `/ucenter/updateUserInfo`,
-      method: "post",
-      data: userData
-    });
+      method: 'post',
+      data: userData,
+    })
   },
 
   // 修改用户密码
   changeUserPwd(pwdData) {
     return request({
       url: `/ucenter/changeUserPwd`,
-      method: "post",
-      data: pwdData
-    });
+      method: 'post',
+      data: pwdData,
+    })
   },
 
   // 获取编辑器图片上传token
   getUploadImageToken() {
     return request({
       url: `/eduask/uploadqiniutoken`,
-      method: "post"
-    });
-  }
-};
+      method: 'post',
+    })
+  },
+}
--- pages/user/_id/_type.vue
+++ pages/user/_id/_type.vue
@@ -35,17 +35,101 @@
 		<div class="container">
 			<div class="col-md-8">
 				<div class="ucenter-home-content">
-					<el-tabs v-model="parmType" @tab-click="userTypeClick" :tab-position="tabPosition">
-						<el-tab-pane label="文章" name="blog">
-							<div class="user_article_content book_item">
-								<div class="nodata-warper" v-if="articleList.length == 0">
+					<div class="list-header">
+						<div class="header-content">
+							<nuxt-link :to="'/user/' + userInfo.id + '/blog'" class="nav-item"
+								v-bind:class="{ active: parmType == 'blog' }">
+								<div class="item-title">文章</div>
+							</nuxt-link>
+							<nuxt-link :to="'/user/' + userInfo.id + '/collect-blog'" class="nav-item"
+								v-bind:class="{ active: parmType.substring(0, 7) == 'collect' }">
+								<div class="item-title">收藏</div>
+							</nuxt-link>
+							<nuxt-link :to="'/user/' + userInfo.id + '/focus'" class="nav-item"
+								v-bind:class="{ active: parmType == 'focus' }">
+								<div class="item-title">关注</div>
+							</nuxt-link>
+							<a href="/user/43636194286093/collections" class="nav-item">
+								<div class="item-title">收藏集</div>
+								<div class="item-count only-in-scroll-mode">2</div>
+							</a><a href="/user/43636194286093/tags" class="nav-item">
+								<div class="item-title">关注</div>
+							</a>
+
+						</div>
+
+					</div>
+					<div class="list-body">
+						<div class="user_article_content book_item" v-if="parmType == 'blog'">
+							<div class="nodata-warper" v-if="dataList.length == 0">
+								<img class="nodata-image-tips" src="https://img.redskt.com/asset/img/nodata.png" />
+								<div>
+									<span>该用户暂时还没有发表文章哦</span>
+								</div>
+							</div>
+							<ul class="article_list">
+								<li v-for="bitem in dataList" :key="bitem.id">
+									<div class="op_artie_content" v-if="bitem.ctype === 1">
+										<nuxt-link class="article_title" :to="'/practice/' + bitem.id">
+											{{ bitem.title }}
+										</nuxt-link>
+										<p class="op_pratice_describ">{{ bitem.descrb }}</p>
+										<ul>
+											<i class="pratice_icon_view"></i>
+											<span class="icon_des">{{ bitem.viewCount }}</span>
+											<i class="pratice_icon_zhan"></i>
+											<span class="icon_des">{{ bitem.good }}</span>
+											<i class="pratice_icon_comment"></i>
+											<span class="icon_des">{{ bitem.ccount }}</span>
+										</ul>
+									</div>
+									<div class="op_message_content" v-else>
+										<div class="push-lf">
+											<nuxt-link :to="'/about/detail/' + bitem.id">
+												<img :src="bitem.cover" />
+											</nuxt-link>
+										</div>
+
+										<div class="push-rt">
+											<p class="message-title">
+												<nuxt-link :to="'/about/detail/' + bitem.id" target="_blank">{{ bitem.title }}</nuxt-link>
+											</p>
+											<div class="message-info">
+												<div class="info-item">
+													<i class="el-icon-view"></i>
+													<span class="read-tx">{{ bitem.viewCount }} 阅读</span>
+												</div>
+											</div>
+										</div>
+										<div class="clearfix">
+										</div>
+									</div>
+								</li>
+							</ul>
+						</div>
+						<div class="user-collect-content" v-if="parmType.substring(0, 7) == 'collect'">
+							<div class="sub-header">
+								<div class="sub-header-title">收藏</div>
+								<div class="sub-type-box">
+									<nuxt-link :to="'/user/' + userInfo.id + '/collect-blog'" class="sub-type"
+										v-bind:class="{ active: parmType.substring(8, 12) == 'blog' }">
+										文章
+									</nuxt-link>
+									<nuxt-link :to="'/user/' + userInfo.id + '/collect-ask'" class="sub-type"
+										v-bind:class="{ active: parmType.substring(8, 11) == 'ask' }">
+										问题
+									</nuxt-link>
+								</div>
+							</div>
+							<div class="collect-content-list book_item" v-if="parmType.substring(8, 12) == 'blog'">
+								<div class="nodata-warper" v-if="dataList.length == 0">
 									<img class="nodata-image-tips" src="https://img.redskt.com/asset/img/nodata.png" />
 									<div>
-										<span>该用户暂时还没有文章哦</span>
+										<span>该用户暂时还没有收藏文章哦</span>
 									</div>
 								</div>
 								<ul class="article_list">
-									<li v-for="bitem in articleList" :key="bitem.id">
+									<li v-for="bitem in dataList" :key="bitem.id">
 										<div class="op_artie_content" v-if="bitem.ctype === 1">
 											<nuxt-link class="article_title" :to="'/practice/' + bitem.id">
 												{{ bitem.title }}
@@ -84,10 +168,57 @@
 									</li>
 								</ul>
 							</div>
-						</el-tab-pane>
-						<el-tab-pane label="收藏" name="collect">收藏</el-tab-pane>
-						<el-tab-pane label="关注" name="focus">关注</el-tab-pane>
-					</el-tabs>
+
+							<div class="collect-content-list ask-item" v-if="parmType.substring(8, 11) == 'ask'">
+								<div class="nodata-warper" v-if="dataList.length == 0">
+									<img class="nodata-image-tips" src="https://img.redskt.com/asset/img/nodata.png" />
+									<div>
+										<span>该用户暂时还没有收藏问题哦</span>
+									</div>
+								</div>
+								<div class="questions_collect_con">
+									<div v-for="item in dataList" :key="item.qid" class="question_list">
+										<div class="answer_title">
+											<nuxt-link :to="'/faquestion/' + item.qid" class="header">
+												{{ item.title }}
+												<div class="ui red label horizontal" data-tooltip="置顶">顶</div>
+												<div class="ui orange label horizontal" data-tooltip="热门">热</div>
+											</nuxt-link>
+										</div>
+
+										<div class="description">
+											<p class="line-clamp" v-html="item.content"></p>
+										</div>
+										<div class="extra question-tags">
+											<a class="ui horizontal basic label popup-tag" href="https://www.oschina.net/question/tag/ruby"
+												target="_blank">
+												<img src="https://static.oschina.net/img/logo/ruby.gif" />Ruby
+											</a>
+
+											<a class="ui horizontal basic label popup-tag" href="https://www.oschina.net/question/tag/java"
+												target="_blank">
+												<img src="https://static.oschina.net/img/logo/java.png" />Java
+											</a>
+										</div>
+
+										<div class="q_time">
+											<span>{{ item.gmtCreate }}来自</span>
+											<a class="user_name" href="https://my.csdn.net/dabocaiqq" target="_blank">{{ item.nickname }}</a>
+											<b>
+												悬赏
+												<a href="javascript:;" title="个人悬赏">{{ item.price }}C</a>
+											</b>
+											<nuxt-link :to="'/faquestion/' + item.qid" class="answer_num" title="问题回答数量">
+												<span>{{ item.reply }}</span>
+												<p class="anser-lable">回答</p>
+											</nuxt-link>
+										</div>
+									</div>
+								</div>
+
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 			<div class="col-md-4 ucenter-rightinfo">
@@ -114,9 +245,9 @@
 										d="M4 12.5S6.917 7 12.75 7s8.75 5.5 8.75 5.5-2.917 5.5-8.75 5.5S4 12.5 4 12.5zm8.75 2.292c1.208 0 2.188-1.026 2.188-2.292 0-1.266-.98-2.292-2.188-2.292-1.208 0-2.188 1.026-2.188 2.292 0 1.266.98 2.292 2.188 2.292z">
 									</path>
 								</g>
-							</svg> <span class="content">文章被阅读<span class="count">{{ userInfo.viewsum }}</span></span></div><a
-							href="https://juejin.cn/book/6844733795329900551/section/6844733795371843597" target="_blank"
-							rel="nofollow noopener noreferrer" class="stat-item" data-v-4fa4e5da=""><svg
+							</svg> <span class="content">文章被阅读<span class="count">{{ userInfo.viewsum }}</span></span></div>
+
+						<nuxt-link :to="'/about/detail/1523552231247499264'" target="_blank" class="stat-item"><svg
 								xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" class="icon stat-jp-icon">
 								<g fill="none" fill-rule="evenodd">
 									<circle cx="12.5" cy="12.5" r="12.5" fill="#E1EFFF"></circle>
@@ -124,7 +255,7 @@
 										d="M16.694 13.516l-3.719 3.055a1.1 1.1 0 0 1-1.412-.013l-2.77-2.362-3.597 2.437a.693.693 0 0 1-.895-.101.649.649 0 0 1-.008-.876l3.68-4.096a1.1 1.1 0 0 1 1.507-.122l2.653 2.135 2.248-2.4-1.34-1.358a.5.5 0 0 1 .327-.85l5.438-.313a.5.5 0 0 1 .528.533l-.368 5.449a.5.5 0 0 1-.855.317l-1.417-1.435z">
 									</path>
 								</g>
-							</svg> <span class="content">源力值<span class="count">{{ userInfo.practice }}</span></span></a>
+							</svg> <span class="content">源力值<span class="count">{{ userInfo.practice }}</span></span></nuxt-link>
 						<!---->
 					</div>
 				</div>
@@ -169,15 +300,15 @@ export default {
 		return {
 			isLogin: false,
 			loginInfo: {},
-			articleList: [], // 查询表单对象
 			isFocus: false,
 			isSetting: false,
 		};
 	},
 
 	asyncData ({ params, error, query }) {
-		return userServerApi.getShowUserInfo(params.id).then(response => {
+		return userServerApi.getShowUserInfo(params.id, params.type).then(response => {
 			return {
+				dataList: response.data.dataList,
 				userInfo: response.data.userInfo,
 				parmUid: params.id,
 				parmType: params.type
@@ -185,9 +316,6 @@ export default {
 		});
 	},
 	mounted () {
-		userServerApi.getShowUserInfo(this.parmUid).then(response => {
-		});
-		this.getUserArticleList();
 		var token = localStorage.getItem("redclass_token");
 		var userStr = localStorage.getItem("redclass_user");
 		if (
@@ -206,13 +334,20 @@ export default {
 	},
 
 	methods: {
-		userTypeClick (tab, event) {
-		},
 		// 获取文章列表的方法
 		getUserArticleList () {
-			userApi.getUserArticleList(this.parmUid).then(response => {
+			userApi.getArticleList(this.parmUid).then(response => {
 				this.articleList = response.data.articleList;
 			});
+		},
+
+		getUserCollectArticleList () {
+			userApi.getCollectArticleList(this.parmUid).then(response => {
+				//this.articleList = response.data.articleList;
+			});
+		},
+		focusUserClick () {
+
 		},
 		personSetting () {
 			var loginToken = window.localStorage.getItem('redclass_token');
@@ -220,6 +355,12 @@ export default {
 				name: "user-setting",
 				params: { "loginToken": loginToken },
 			});
+		},
+		getCheckType (type) {
+			if (type.substring(0, 7) == 'collect') {
+				return true;
+			}
+			return false
 		}
 	},
 
@@ -235,6 +376,193 @@ export default {
 </script>
 
 <style>
+.questions_collect_con {
+	margin-top: 20px;
+}
+
+.answer_num .anser-lable {
+	margin-top: 0px;
+}
+
+.questions_collect_con .q_time {
+	margin-top: 10px;
+	margin-bottom: 10px;
+	position: relative;
+}
+
+.questions_collect_con .answer_num {
+	width: 50px;
+	height: 50px;
+	background: #a0d56d;
+	position: absolute;
+	right: 0px;
+	top: -30px;
+	border-radius: 3px;
+	font-size: 14px;
+	color: #fff;
+	text-align: center;
+	cursor: pointer;
+	padding-top: 5px;
+}
+
+.questions_collect_con .extra img {
+	display: inline-block;
+	vertical-align: baseline;
+	height: 22px !important;
+	border-radius: 0.14285714rem;
+	padding: 2px 0;
+	margin: -0.5833em 0.5em -0.5833em 0;
+}
+
+.questions_collect_con .extra {
+	display: block;
+	position: relative;
+	background: 0 0;
+	margin: 0.5rem 0 0;
+	width: 100%;
+	padding: 0 0 0;
+	top: 0;
+	left: 0;
+	color: rgba(0, 0, 0, 0.4);
+	box-shadow: none;
+	transition: color 0.1s ease;
+	border-top: none;
+}
+
+.questions_collect_con .description {
+	font-size: 14px;
+	margin-top: 10px;
+	margin-bottom: 10px;
+	overflow: hidden;
+	text-overflow: ellipsis;
+	white-space: nowrap;
+	vertical-align: middle;
+	max-height: 40px;
+}
+
+.questions_collect_con .header {
+	display: inline-block;
+	text-decoration: none;
+	margin: -0.21425em 0 0;
+	font-family: 'PingFang SC', 'Helvetica Neue', 'Microsoft YaHei UI', 'Microsoft YaHei', 'Noto Sans CJK SC', Sathu, EucrosiaUPC, Arial, Helvetica, sans-serif;
+	font-weight: 700;
+	color: rgba(0, 0, 0, 0.85);
+}
+
+.questions_collect_con .question_list {
+	margin-bottom: 20px;
+	border-bottom: 1px solid #f0f0f0;
+	margin-right: 15px;
+}
+
+.questions_collect_con .question_list .answer_title {
+	font-size: 14px;
+}
+
+.list-body .sub-header .sub-type-box .sub-type.active {
+	color: #000;
+}
+
+.ucenter-home-content .list-body {
+	margin-top: 0px;
+}
+
+.list-body .sub-header .sub-type-box .sub-type:not(:last-child) {
+	margin-right: 26px;
+}
+
+.list-body .sub-header .sub-type-box .sub-type:not(:last-child):after {
+	content: "";
+	position: absolute;
+	top: 50%;
+	right: -1rem;
+	margin-top: -0.5em;
+	width: 1px;
+	height: 1em;
+	background-color: #b2bac2;
+	opacity: .5;
+}
+
+.list-body .sub-header .sub-type-box .sub-type {
+	position: relative;
+	padding: 1rem 0;
+	font-size: 16px;
+	color: #72777b;
+	text-decoration: none;
+}
+
+.list-body .sub-header .sub-type-box {
+	margin-left: auto;
+}
+
+.list-body .sub-header .sub-header-title {
+	margin-right: 1rem;
+	font-size: 16px;
+	font-weight: 550;
+	color: #000;
+}
+
+.list-body .sub-header {
+	display: flex;
+	align-items: center;
+	flex-wrap: wrap;
+	padding: 0 24px;
+	height: 56px;
+	white-space: nowrap;
+	border-bottom: 2px solid rgba(230, 230, 231, .5);
+}
+
+.list-header .nav-item {
+	flex: 0 0 auto;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	position: relative;
+	width: 70px;
+	height: 100%;
+	text-decoration: none;
+}
+
+.list-header .nav-item.active:after {
+	content: "";
+	position: absolute;
+	width: 22px;
+	height: 3px;
+	border-radius: 50px;
+	left: 50%;
+	bottom: 0;
+	transform: translateX(-50%);
+	z-index: 10;
+	background-color: #1e80ff;
+}
+
+.list-header .nav-item .item-title {
+	font-size: 18px;
+	font-weight: 400;
+	color: #515767;
+}
+
+.list-header {
+	position: relative;
+	margin: 0;
+	padding: 0;
+	height: 60px;
+	background-color: #fff;
+	border-radius: 2px 2px 0 0;
+	border-bottom: 1px solid #e4e6eb;
+	z-index: 100;
+}
+
+.list-header .header-content {
+	display: flex;
+	align-items: center;
+	height: 100%;
+	max-width: 960px;
+	white-space: nowrap;
+	position: relative;
+	margin: 0 auto;
+}
+
 .more-block .more-item .item-title {
 	margin-right: auto;
 }
@@ -406,7 +734,7 @@ export default {
 
 .user-info {
 	float: left;
-	margin-top: 74px;
+	margin-top: 58px;
 }
 
 .user-info .user-pic {

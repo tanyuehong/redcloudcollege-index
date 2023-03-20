81d0621693258768026e1f96117f339f3500e4de
tanyuehong <tanyuehong@zuoyebang.com>
2023-03-20 14:04:07 +0800

    每日一题开发

--- pages/user/admin/interview/question.vue
+++ pages/user/admin/interview/question.vue
@@ -27,7 +27,7 @@
                 d="M9.58901 0.903176C9.07116 0.786587 8.53915 0.727234 7.99989 0.727234C7.46051 0.727234 6.92838 0.786615 6.41041 0.903258L6.11154 0.970562L5.17999 3.09499L2.88551 2.84416L2.67809 3.07008C1.95184 3.86112 1.40698 4.80463 1.08759 5.83506L0.997559 6.12553L2.36299 7.99996L1.17661 9.62859L0.997559 9.87439L1.08759 10.1649C1.40698 11.1953 1.95184 12.1388 2.67809 12.9298L2.88551 13.1558L5.17999 12.9049L5.98852 14.7488L6.11154 15.0294L6.41041 15.0967C6.92838 15.2133 7.46051 15.2727 7.99989 15.2727C8.53915 15.2727 9.07116 15.2133 9.58901 15.0967L9.88786 15.0295L10.8198 12.9049L13.1143 13.1558L13.3217 12.9298C14.0479 12.1389 14.5927 11.1955 14.9121 10.1652L15.0021 9.87479L13.6368 7.99996L14.8231 6.37093L15.0021 6.12513L14.9121 5.83469C14.5927 4.80441 14.0479 3.86103 13.3217 3.07008L13.1143 2.84416L10.8198 3.095L9.88786 0.970458L9.58901 0.903176ZM6.97185 2.02705C7.30885 1.96884 7.65237 1.93931 7.99995 1.93931C8.34745 1.93931 8.69087 1.96882 9.02778 2.027L9.7747 3.72978C9.94877 4.1266 10.3602 4.36453 10.791 4.31745L12.6271 4.11672C13.0706 4.65049 13.4202 5.257 13.6589 5.91059L12.5627 7.41589C12.3092 7.76397 12.3092 8.23586 12.5627 8.58396L13.6589 10.0892C13.4202 10.7428 13.0706 11.3493 12.6271 11.8831L10.791 11.6824C10.3602 11.6353 9.94877 11.8732 9.7747 12.27L9.02778 13.9728C8.69087 14.031 8.34745 14.0605 7.99995 14.0605C7.65237 14.0605 7.30885 14.031 6.97185 13.9728L6.22524 12.2701C6.05114 11.8732 5.63967 11.6353 5.20892 11.6824L3.3728 11.8831C2.92928 11.3493 2.5796 10.7427 2.3409 10.089L3.43719 8.58401C3.69075 8.23586 3.69075 7.76397 3.43723 7.41588L2.3409 5.91086C2.5796 5.25717 2.92928 4.65056 3.3728 4.11672L5.20891 4.31745C5.63967 4.36453 6.05114 4.1266 6.2252 3.72979L6.97185 2.02705ZM7.99996 4.96962C9.66782 4.96962 11.0187 6.32698 11.0187 7.99993C11.0187 9.67287 9.66782 11.0302 7.99996 11.0302C6.3321 11.0302 4.9812 9.67287 4.9812 7.99993C4.9812 6.32698 6.3321 4.96962 7.99996 4.96962ZM6.19336 7.99992C6.19336 6.99512 7.00287 6.18174 8 6.18174C8.99713 6.18174 9.80663 6.99512 9.80663 7.99992C9.80663 9.00472 8.99713 9.81811 8 9.81811C7.00287 9.81811 6.19336 9.00472 6.19336 7.99992Z"
                 class="close-path"></path>
             </svg>
-            账号设置
+            每日一题
           </div>
         </div>
         <div @click="authoritySettingClick" class="setting-menu-item" v-if="userInfo.authority == 100">
@@ -40,6 +40,73 @@
         <div class="person-setting shadow">
           <div class="nav-text">面试题列表</div>
           <div class="admin-content">
+            <el-table :data="questionList" height="560">
+              <el-table-column property="title" label="题目名称" width="260"></el-table-column>
+              <el-table-column property="type" label="题目类型" width="120"></el-table-column>
+              <el-table-column label="操作" width="290">
+                <template slot-scope="scope">
+                  <el-button size="mini" @click="editQuestionClick(scope.$index, scope.row)">编辑</el-button>
+                  <el-button size="mini" @click="editQuestionClassifyClick(scope.$index, scope.row)">职位</el-button>
+                  <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+
+            <el-dialog :title="functionTitle" width="140" :close-on-click-modal="false" :close-on-press-escape="false"
+              :show-close="true" :visible.sync="showQustionPositionPage" center>
+              <el-table :data="questionClassifyList" height="160" v-if="classifyType == 1">
+                <el-table-column property="" label="面试题Title" width="160" height="60"><span>{{ editQuestion.title }}</span></el-table-column>
+                <el-table-column property="pname" label="面试职位" width="100" height="60"></el-table-column>
+                <el-table-column property="sname" label="子分类" width="100" height="60"></el-table-column>
+                <el-table-column label="操作" width="220">
+                  <template slot-scope="scope">
+                    <el-button size="mini" @click="editPositionClassifyClick(scope.$index, scope.row)">编辑</el-button>
+                    <el-popconfirm confirm-button-text="好的" cancel-button-text="不用了" icon="el-icon-info" icon-color="red"
+                      title="您确定要删除该分类吗，该操作不可撤回？" @confirm="deleteClassify(scope.$index, scope.row)">
+                      <el-button type="danger" size="mini" slot="reference">删除</el-button>
+                    </el-popconfirm>
+                  </template>
+                </el-table-column>
+              </el-table>
+
+              <el-form ref="form" label-width="80px" v-if="classifyType == 2">
+                <el-form-item label="面试题Title">
+                  <span>{{ editQuestion.title }}</span>
+                </el-form-item>
+                <el-form-item label="职位名称">
+                  <el-select v-model="editQuestionClassify.pid" placeholder="请选择" @change="selectPositionChanged">
+                    <el-option v-for="position in positionList" :key="position.id" :label="position.name"
+                      :value="position.id">
+                    </el-option>
+                  </el-select>
+                </el-form-item>
+                <el-form-item label="子分类">
+                  <el-select v-model="editQuestionClassify.sid" clearable placeholder="请选择">
+                    <el-option v-for="classify in classifyList" :key="classify.id" :label="classify.name"
+                      :value="classify.id">
+                    </el-option>
+                  </el-select>
+                </el-form-item>
+              </el-form>
+              <span slot="footer" class="dialog-footer">
+                <el-button @click="positionBackClick" size="small">返 回</el-button>
+                <el-button type="primary" @click="onSubmitClick" size="small" v-if="submitTitle.length > 0">{{ submitTitle
+                }}</el-button>
+              </span>
+            </el-dialog>
+          </div>
+        </div>
+      </div>
+
+      <div v-if="settingtype === 2">
+        <div class="person-setting shadow">
+          <div class="nav-text">每日一题管理</div>
+          <div class="admin-content">
+            <el-date-picker
+      v-model="value1"
+      type="date"
+      placeholder="选择日期">
+    </el-date-picker>
             <el-table :data="questionList" height="560">
               <el-table-column property="title" label="题目名称" width="260"></el-table-column>
               <el-table-column property="type" label="题目类型" width="120"></el-table-column>
@@ -126,21 +193,6 @@ export default {
 
       userInfo: {}, // 查询表单对象
       settingtype: 1,
-      submitUserInfo: false,
-      isUploadHiden: true,
-      zhuxiaodlog: false,
-      pwdChange: false,
-      oldPwd: "",
-      oldPwdTips: "",
-      newPwd: "",
-      newPwdTips: "",
-      confirmPwd: "",
-      confirmPwdTips: "",
-      resultTips: "",
-      submitChangePwd: false,
-      changephine: false,
-      loginToken: "",
-      touxiang: ""
     };
   },
 
